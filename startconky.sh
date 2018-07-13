a78f17a5909a0a1f5e4fbbca75b071f4a7e9f375
9527tech <2320874815@qq.com>
2018-07-13 12:04:12 +0800

    将internet更名为network

--- startconky.sh
+++ startconky.sh
@@ -5,7 +5,7 @@ conky -c ~/.Conky/rings -d & # the main conky with rings
 #sleep 1 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
 conky -c ~/.Conky/cpu -d &
 conky -c ~/.Conky/mem -d &
-conky -c ~/.Conky/internet -d &
+conky -c ~/.Conky/network -d &
 conky -c ~/.Conky/clock -d &
 conky -c ~/.Conky/arch_logo -d &
 #conky -c ~/.Conky/notes &
